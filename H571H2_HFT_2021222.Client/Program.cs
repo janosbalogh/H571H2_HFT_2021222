b8ae1ae56f62343ec5ecfea9d830ca2cee636efa
Balogh János <balogh.janos@stud.uni-obuda.hu>
2022-03-26 16:02:37 +0100

    Add project files.

---
+++ H571H2_HFT_2021222.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace H571H2_HFT_2021222.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
