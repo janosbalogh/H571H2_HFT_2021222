e90f1128e5f1b84caad67575649095cbf1ddba54
balogh.janos <balogh.janos@stud.uni-obuda.hu>
2022-04-29 01:34:35 +0200

    Added NuGet EF packages DbContext implemented w/o the DbSeed

--- H571H2_HFT_2021222.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace H571H2_HFT_2021222.Repository
-{
-    public class Class1
-    {
-    }
-}
--- H571H2_HFT_2021222.Repository/SteamDbContext.cs
+++ H571H2_HFT_2021222.Repository/SteamDbContext.cs
@@ -1,9 +1,69 @@
-﻿using System;
+﻿using H571H2_HFT_2021222.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace H571H2_HFT_2021222.Repository
 {
-    public class SteamDbContext:DbContext
+    public class SteamDbContext : DbContext
     {
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Person> People { get; set; }
+
+        public SteamDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseInMemoryDatabase("SteamDb")
+                    .UseLazyLoadingProxies();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Game>()
+                .HasOne(game => game.Company)
+                .WithMany(game => game.Game)
+                .HasForeignKey(game => game.CompanyID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Company>()
+                .HasOne(company => company.Executive)
+                .WithOne(company => company.Company)
+                .HasForeignKey<Person>(company => company.CompanyID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+            //Db Seed
+
+            modelBuilder.Entity<Company>().HasData(new Company[]
+            {
+                
+
+            });
+            modelBuilder.Entity<Game>().HasData(new Game[]
+            {
+                
+
+
+            });
+            modelBuilder.Entity<Person>().HasData(new Person[]
+            {
+                
+            });
+
+
+        }
+
+
+
+
 
     }
 }
