a2a38ae943129e8284a86b4137d621346c576c76
balogh.janos <balogh.janos@stud.uni-obuda.hu>
2022-05-03 00:26:18 +0200

    Interfaces created and implemented

---
+++ H571H2_HFT_2021222.Logic/Interfaces/ICompanyLogic.cs
@@ -0,0 +1,18 @@
+﻿using H571H2_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H571H2_HFT_2021222.Logic
+{
+    public interface ICompanyLogic
+    {
+        void Create(Company item);
+        Company Read(int id);
+        IQueryable<Company> ReadAll();
+        void Update(Company item);
+        void Delete(int id);
+    }
+}
---
+++ H571H2_HFT_2021222.Logic/Interfaces/IPersonLogic.cs
@@ -0,0 +1,18 @@
+﻿using H571H2_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H571H2_HFT_2021222.Logic
+{
+    public interface IPersonLogic
+    {
+        void Create(Person item);
+        Person Read(int id);
+        IQueryable<Person> ReadAll();
+        void Update(Person item);
+        void Delete(int id);
+    }
+}
