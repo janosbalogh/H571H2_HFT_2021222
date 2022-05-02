387a8c3c66b89bc339c0e4144a8515591907588a
balogh.janos <balogh.janos@stud.uni-obuda.hu>
2022-05-03 00:23:22 +0200

    Interface created

---
+++ H571H2_HFT_2021222.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace H571H2_HFT_2021222.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        void Create(T item);
+        T Read(int id);
+        IQueryable<T> ReadAll();
+        void Update(T item);        
+        void Delete(int id);
+
+    }
+}
