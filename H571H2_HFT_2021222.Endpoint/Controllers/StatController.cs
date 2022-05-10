d3c38b61631b16f8d540d909bea6b5f86e2ba669
balogh.janos <balogh.janos@stud.uni-obuda.hu>
2022-05-11 00:32:06 +0200

    StatController crated and implemented a noncrud

---
+++ H571H2_HFT_2021222.Endpoint/Controllers/StatController.cs
@@ -0,0 +1,31 @@
+﻿using H571H2_HFT_2021222.Logic.Classes;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace H571H2_HFT_2021222.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        IGameLogic logic;
+
+        public StatController(IGameLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, int>> Top3CompanyWithMostGames()
+        {
+            return this.logic.Top3CompanyWithMostGames();
+        }
+
+        //TODO: List companies that made FPS games
+    }
+}
