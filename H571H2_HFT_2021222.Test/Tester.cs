9135c4f588cfcd117c99c9eff2f9db7b825a4b28
balogh.janos <balogh.janos@stud.uni-obuda.hu>
2022-05-11 16:47:47 +0200

    created test class

---
+++ H571H2_HFT_2021222.Test/Tester.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Moq;
+using H571H2_HFT_2021222.Logic;
+using H571H2_HFT_2021222.Models;
+using H571H2_HFT_2021222.Repository;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using H571H2_HFT_2021222.Logic.Classes;
+
+namespace H571H2_HFT_2021222.Test
+{
+    [TestFixture]
+    public class Tester
+    {
+        CompanyLogic companyLogic;
+        GameLogic gameLogic;
+        Person personLogic;
+
+        Mock<IRepository<Company>> mockCompanyRepo;
+        Mock<IRepository<Game>> mockGameRepo;
+        Mock<IRepository<Person>> mockPersonRepo;
+
+        [SetUp]
+        public void Init()
+        {
+
+        }
+    }
+}
