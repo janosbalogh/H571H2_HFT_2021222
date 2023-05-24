2950a5f848cc64b83c6421e0b8a1316e5c6391be
balogh.janos <balogh.janos@stud.uni-obuda.hu>
2023-05-24 23:12:43 +0200

    basic UI created

---
+++ H571H2_HFT_2021222.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using H571H2_HFT_2021222.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H571H2_HFT_2021222.WpfClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Company> Companies { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Companies = new RestCollection<Company>("http://localhost:38231/", "company");
+        }
+
+    }
+}
