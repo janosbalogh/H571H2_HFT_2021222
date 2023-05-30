d37154854f406733cf2ab52656291235cbda0f42
balogh.janos <balogh.janos@stud.uni-obuda.hu>
2023-05-30 21:21:03 +0200

    MainWindowViewModel implemented

--- H571H2_HFT_2021222.WpfClient/MainWindowViewModel.cs
+++ H571H2_HFT_2021222.WpfClient/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using H571H2_HFT_2021222.Models;
+using H571H2_HFT_2021222.WpfClient.ViewModels;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
 using System;
@@ -14,42 +15,31 @@ namespace H571H2_HFT_2021222.WpfClient
 {
     public class MainWindowViewModel:ObservableRecipient
     {
-        private string errorMessage;
 
-        public string ErrorMessage
-        {
-            get { return errorMessage; }
-            set { SetProperty(ref errorMessage, value); }
-        }
-        public RestCollection<Company> Companies { get; set; }
+        public CompanyViewModel companyView { get; set; }
+        public GameViewModel gameView { get; set; }
+        public PersonViewModel personView { get; set; }
 
-        private Company selectedCompany;
-
-        public Company SelectedCompany
-        {
-            get { return selectedCompany; }
-            set 
-            {
-                if (value != null)
-                {
-                    selectedCompany = new Company()
-                    {
-                        Name = value.Name,
-                        CompanyID = value.CompanyID
-                    };
-                    OnPropertyChanged();
-                    (DeleteCompanyCommand as RelayCommand).NotifyCanExecuteChanged();
-                    
-                }
-                
-            }
-        }
-
-
-        public ICommand CreateCompanyCommand { get; set; }
-        public ICommand DeleteCompanyCommand { get; set; }
-        public ICommand UpdateCompanyCommand { get; set; }
+        static RestService restService;
         
+        void Visibilityhiding()
+        {
+            companyView.CreateCompanyVisibility = Visibility.Hidden;
+            companyView.UpdateCompanyVisibility = Visibility.Hidden;
+            
+            gameView.CreateGameVisibility = Visibility.Hidden;
+            gameView.UpdateGameVisibility = Visibility.Hidden;
+
+            personView.CreatePersonVisibility = Visibility.Hidden;
+            personView.UpdatePersonVisibility = Visibility.Hidden;
+
+            companyView.CompanyVisibility = Visibility.Hidden;
+            gameView.GameVisibility = Visibility.Hidden;
+            personView.PersonVisibility = Visibility.Hidden;
+
+        }
+
+
         public static bool IsInDesignMode
         {
             get
@@ -60,44 +50,94 @@ namespace H571H2_HFT_2021222.WpfClient
         }
         public MainWindowViewModel()
         {
+            companyView = new CompanyViewModel();
+            gameView = new GameViewModel();
+            personView = new PersonViewModel();
+
+            Visibilityhiding();
+
             if (!IsInDesignMode)
             {
-                Companies = new RestCollection<Company>("http://localhost:38231/", "company", "hub");
+                companyView.Companies = new RestCollection<Company>("http://localhost:38231/", "company", "hub");
+                gameView.Games = new RestCollection<Game>("http://localhost:38231/", "game", "hub");
+                personView.People = new RestCollection<Person>("http://localhost:38231/", "person", "hub");
 
-                CreateCompanyCommand = new RelayCommand(() =>
+                restService = new RestService("http://localhost:38231/", "game");
+
+
+                companyView.ShowCompanyTable = new RelayCommand(() =>
                 {
-                    Companies.Add(new Company()
-                    {
-                        Name = SelectedCompany.Name,
-                        
-                    });
-
+                    Visibilityhiding();
+                    companyView.CompanyVisibility = Visibility.Visible;
                 });
 
-                UpdateCompanyCommand = new RelayCommand(() =>
+                gameView.ShowGameTable = new RelayCommand(() =>
                 {
-                    try
+                    Visibilityhiding();
+                    gameView.GameVisibility = Visibility.Visible;
+                });
+
+                personView.ShowPersonTable = new RelayCommand(() =>
+                {
+                    Visibilityhiding();
+                    personView.PersonVisibility = Visibility.Visible;
+                });
+
+                gameView.CompaniesWithFpsGamesCommand = new RelayCommand(() =>
+                {
+                    IEnumerable<KeyValuePair<string, int>> q = restService.Get<KeyValuePair<string, int>>("Stat/CompaniesWithFpsGames");
+                    string l1 = null;
+                    foreach (var item in q)
                     {
-                        Companies.Update(SelectedCompany);
+                        l1 += item.Key.ToString() + "\n";
                     }
-                    catch (ArgumentException ex)
-                    {
-                        ErrorMessage = ex.Message;
+                    MessageBox.Show(l1.ToString(), "CompaniesWithFpsGame");
+                });
+
+                gameView.CompanyNameLongerThan20Command = new RelayCommand(() =>
+                {
+                    IEnumerable<KeyValuePair<string, int>> q = restService.Get<KeyValuePair<string, int>>("Stat/CompanyNameLongerThan20");
+                    string l1 = null;
+                    foreach (var item in q)
+                    {                        
+                        l1 += item.Key.ToString() + "\n";
                     }
-
+                    MessageBox.Show(l1.ToString(), "CompanyNameLongerThan20");
                 });
 
-                DeleteCompanyCommand = new RelayCommand(() =>
+                gameView.Top3CompanyWithMostGamesCommand = new RelayCommand(() =>
                 {
-                    Companies.Delete(SelectedCompany.CompanyID);
-                },
-                () =>
-                {
-                    return SelectedCompany != null;
+                    IEnumerable<KeyValuePair<string, int>> q = restService.Get<KeyValuePair<string, int>>("Stat/Top3CompanyWithMostGames");
+                    string l1 = null;
+                    foreach (var item in q)
+                    {                        
+                        l1 += item.Key.ToString() + ":  " + item.Value.ToString() + "\n";
+                    }
+                    MessageBox.Show(l1.ToString(), "Top3CompanyWithMostGames");
                 });
 
-                SelectedCompany = new Company();
+                gameView.TOP10MostPlayedGamesExecutiveAgeCommand = new RelayCommand(() =>
+                {
+                    IEnumerable<KeyValuePair<string, int>> q = restService.Get<KeyValuePair<string, int>>("Stat/TOP10MostPlayedGamesExecutiveAge");
+                    string l1 = null;
+                    foreach (var item in q)
+                    {                        
+                        l1 += item.Key.ToString() +":  " + item.Value.ToString() + "\n";
+                    }
+                    MessageBox.Show(l1.ToString(), "TOP10MostPlayedGamesExecutiveAge");
+                });
 
+                personView.ExecutiveSalaryAbove1000EmployeeCommand = new RelayCommand(() =>
+                {
+                    IEnumerable<KeyValuePair<string, int>> q = restService.Get<KeyValuePair<string, int>>("Stat/ExecutiveSalaryAbove1000Employee");
+                    string l1 = null;
+                    foreach (var item in q)
+                    {                        
+                        l1 += item.Key.ToString() + ":  " + item.Value.ToString() + "\n";
+                    }
+                    MessageBox.Show(l1.ToString(), "ExecutiveSalaryAbove1000Employee");
+                });
+                
             }
 
         }
