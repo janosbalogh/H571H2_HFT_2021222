d8400d810d8ac23abb4b289df799cc40c9419e30
balogh.janos <balogh.janos@stud.uni-obuda.hu>
2023-06-11 22:36:14 +0200

    CORS fixed

--- H571H2_HFT_2021222.Endpoint/Startup.cs
+++ H571H2_HFT_2021222.Endpoint/Startup.cs
@@ -3,7 +3,9 @@ using H571H2_HFT_2021222.Logic.Classes;
 using H571H2_HFT_2021222.Models;
 using H571H2_HFT_2021222.Repository;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -58,7 +60,21 @@ namespace H571H2_HFT_2021222.Endpoint
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "H571H2_HFT_2021222.Endpoint v1"));
             }
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var exception = context.Features
+                .Get<IExceptionHandlerPathFeature>()
+                .Error;
+                var response = new { Msg = exception.Message };
+                await context.Response.WriteAsJsonAsync(response);
+            }));
 
+            app.UseCors(x => x
+                .AllowCredentials()
+                .AllowAnyMethod()
+                .AllowAnyHeader()                
+                .WithOrigins("http://localhost:53852"));
+            
             app.UseRouting();
 
             app.UseAuthorization();
@@ -69,11 +85,8 @@ namespace H571H2_HFT_2021222.Endpoint
                 endpoints.MapHub<SignalRHub>("/hub");
             });
             
-            app.UseCors(x => x
-                .AllowCredentials()
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .WithOrigins("http://localhost:39141"));
+            
+           
         }
     }
 }
